6bccebd70c57740fdd471cd7b9e7bd6c0bec8af3
Nagesh Bhosle <bhoslenn@gmail.com>
2026-08-20 00:45:28 +0200

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String encode(List<String> strs) {
+        List<String> temp = new ArrayList<>();
+        if (strs.size() == 0) {
+            return "";
+        }
+        for (String s : strs) {
+            if (s.equals("")) {
+                temp.add("EMPTY");
+            } else if (s.equals(",")) {
+                temp.add("COMMA");
+            } else {
+                temp.add(s);
+            }
+        }
+        return temp.stream().collect(Collectors.joining("SEPERATOR"));
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        if (str.equals(""))
+            return result;
+        List<String> temp = Arrays.asList(str.split("SEPERATOR"));
+
+        for (String s : temp) {
+            if (s.equals("EMPTY")) {
+                result.add("");
+            } else if (s.equals("COMMA")) {
+                result.add(",");
+            } else {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+}
